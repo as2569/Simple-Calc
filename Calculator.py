0c28196b668090af1bf16a56b7c98ec908d2058b
as2569 <as2569@njit.edu>
2020-02-24 18:26:40 -0500

    added sqr method

--- Calculator.py
+++ Calculator.py
@@ -11,9 +11,14 @@ class Calculator:
 	def div(self, num1, num2):
 		return num1 / num2
 
+	def sqr(self, num1):
+		return num1 * num1
+
+
 if __name__ == "__main__":
 	c = Calculator()
 	print(c.add(1,2))
 	print(c.sub(4,3))
 	print(c.mul(4,3))
 	print(c.div(0,4))
+	print(c.sqr(4))
