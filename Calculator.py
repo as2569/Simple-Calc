a5bf87d327861bfa3e839406620bd162949b68de
as2569 <as2569@njit.edu>
2020-02-24 19:06:39 -0500

    added inputs for sub, div, mul

--- Calculator.py
+++ Calculator.py
@@ -24,7 +24,11 @@ cont = True
 
 while cont == True:
 	print('1 for add')
+	print('2 for sub')
+	print('3 for mul')
+	print('4 for div')
 	print('q for quit')
+	print('\n')
 
 	choice = input('Enter choice ')
 	if choice == 'q':
@@ -32,10 +36,23 @@ while cont == True:
 	elif choice == '1':
 		n1 = input('Enter first number ')
 		n2 = input('Enter second number ')
-		print(n1 + '+' + n2 + '=' + str(c.add(float(n1), float(n2))))
+		print(n1 + ' + ' + n2 + ' = ' + str(c.add(float(n1), float(n2))))
+	elif choice == '2':
+		n1 = input('Enter first number ')
+		n2 = input('Enter second number ')
+		print(n1 + ' - ' + n2 + ' = ' + str(c.sub(float(n1), float(n2))))
+	elif choice == '3':
+		n1 = input('Enter first number ')
+		n2 = input('Enter second number ')
+		print(n1 + ' * ' + n2 + ' = ' + str(c.mul(float(n1), float(n2))))
+	elif choice == '4':
+		n1 = input('Enter first number ')
+		n2 = input('Enter second number ')
+		print(n1 + ' / ' + n2 + ' = ' + str(c.div(float(n1), float(n2))))
 	else:
 		print('invalid input ')
 
+	print('\n')
 	#print(c.add(1,1))
 	#print(c.sub(4,3))
 	#print(c.mul(4,3))
