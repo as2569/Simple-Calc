18a3b77d23a2396291e9c2cdb9f392fce4175cd0
as2569 <as2569@njit.edu>
2020-03-23 18:56:09 -0400

    task, tested the CVS reader

--- Driver.py
+++ Driver.py
@@ -1,4 +1,8 @@
 from FileReader import FileReader
+from StatsCalc import StatsCalc
 
-fr = FileReader()
-fr.openFile('testCSV.csv')
+#fr = FileReader()
+#fr.openFile('testCSV.csv')
+
+calc = StatsCalc()
+calc.test()
--- FileReader.py
+++ FileReader.py
@@ -1,15 +1,21 @@
 class FileReader:
+	def __init__(self):
+		self.file = None
+		self.lines = None
+		self.currentLine = 1
+
 	def openFile(self, fileName):
-		file = open(fileName, "r")
-		line = file.next() # to ignore header
+		self.file = open(fileName, "r")
+		self.lines = self.file.readlines()
 
 	def getNumRecords(self):
-		count = 0
-		for line in f:
-			count += 1
-		count -= 1 # substract one for header
-		return count
+		#count = 0
+		#for l in self.file:
+		#	count += 1
+		#count -= 1 # substract one for header
+		return len(self.lines) - 1
 
 	def nextLine(self):
-		line = file.next()
+		line = self.lines[self.currentLine]
+		self.currentLine += 1
 		return line
