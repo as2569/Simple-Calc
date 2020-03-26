941bb666165115bbe8bb2f50bc58cba3ba3840c3
as2569 <as2569@njit.edu>
2020-03-26 14:53:03 -0400

    fix, fixed srt root test again

--- tests/calc_srtRoot_test.py
+++ tests/calc_srtRoot_test.py
@@ -7,7 +7,7 @@ def test_calc_sqrRoot():
 	fr.openFile('csvFiles/UnitTestSquareRoot.csv')
 
 	for row in fr.reader:
-		if calc.sqr(row[0]) == row[1]:
+		if calc.sqr(int(row[0])) == float(row[1]):
 			continue
 		else:
 			assert False
