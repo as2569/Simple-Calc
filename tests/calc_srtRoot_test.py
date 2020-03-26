c1a70e460c7a7a4dcb3bcb0a863ef63f506d2d67
as2569 <as2569@njit.edu>
2020-03-26 15:54:40 -0400

    fix, the rounding error

--- tests/calc_srtRoot_test.py
+++ tests/calc_srtRoot_test.py
@@ -7,7 +7,7 @@ def test_calc_sqrRoot():
 	fr.openFile('csvFiles/UnitTestSquareRoot.csv')
 
 	for row in fr.reader:
-		if calc.sqrRoot(int(row['Value 1'])) == float(row['Result']):
+		if calc.sqrRoot(int(row['Value 1'])) == round(float(row['Result']),3):
 			continue
 		else:
 			assert False
