8c3b9848e08fe452b57028f6205db426d312f72f
as2569 <as2569@njit.edu>
2020-03-30 18:29:53 -0400

    fix, new sub test

--- tests/calc_sub_test.py
+++ tests/calc_sub_test.py
@@ -1,6 +1,17 @@
 from Calculator import Calculator
+from FileReader import FileReader
 
 def test_calc_sub():
-	c = Calculator()
-	assert c.sub(5,1) == 4
+	calc = Calculator()
+	fr = FileReader()
+	fr.openFile('csvFiles/UnitTestSubtraction.csv')
+
+	for row in fr.reader:
+		if calc.sub(int(row['Value 2']), int(row['Value 1'])) == int(row['Result']):
+			continue
+		else:
+			assert False
+
+	assert True
+
 
