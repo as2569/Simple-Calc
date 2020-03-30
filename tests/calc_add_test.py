e3204082b1f72135b3844635afe90598d94d485a
as2569 <as2569@njit.edu>
2020-03-30 18:23:04 -0400

    fix, new add test

--- tests/calc_add_test.py
+++ tests/calc_add_test.py
@@ -1,7 +1,17 @@
 from Calculator import Calculator
+from FileReader import FileReader
 
 def test_calc_add():
-	c = Calculator()
-	assert c.add(2,2) == 4
+	calc = Calculator()
+	fr = FileReader()
+	fr.openFile('csvFiles/UnitTestAddition.csv')
+
+	for row in fr.reader:
+		if calc.add(int(row['Value 1']), int(row['Value 2'])) == int(row['Result']):
+			continue
+		else:
+			assert False
+
+	assert True
 
 
