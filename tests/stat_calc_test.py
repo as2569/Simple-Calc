dc90ebf4c6dcadfa3a9ff6f8e9eabbb01cecc97f
as2569 <as2569@njit.edu>
2020-03-31 00:54:31 -0400

    fix, unit test

--- tests/stat_calc_test.py
+++ tests/stat_calc_test.py
@@ -13,7 +13,7 @@ def test_stats_calc():
 		assert False
 	if statsCalc.standardDev() != 2897.721:
 		assert False
-	if statsCalc.variance != 8396786.683:
+	if statsCalc.variance() != 8396786.683:
 		assert False
 	assert True
 
